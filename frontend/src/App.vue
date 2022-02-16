99b1fb78138684958a9d0f162fc571e8413cc4b9
Leo Mhango <46033545+Leomhango@users.noreply.github.com>
2022-02-16 18:51:04 +0530

    frontend setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,15 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <section class="flex flex-col justify-center items-center">
+    <nav class="flex justify-center item-center py-5 shadow-lg w-full text-lg text-gray-900">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+
+  </section>
+    
+
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <p class="text-3xl">{{string}}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      string: 'Hello, world'
+    }
+  }
+}
+</script>
